70f53a1473d8d8ca5fb89a0543972c26e7f09ea2
Mathieu Hays <5703113+mathieuhays@users.noreply.github.com>
2024-05-21 23:05:29 +0200

    html node test, rename test class

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -3,7 +3,7 @@ import unittest
 from htmlnode import HTMLNode
 
 
-class TestTextNode(unittest.TestCase):
+class TestHTMLNode(unittest.TestCase):
     def test_prop_to_html(self):
         node = HTMLNode(props={"href": "https://www.boot.dev", "target": "_blank"})
         self.assertEqual(' href="https://www.boot.dev" target="_blank"', node.props_to_html())
